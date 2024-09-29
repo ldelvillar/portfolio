3e308f3a041b9df78353b102c8b9f8dc1d8beb42
100495726 <ldelvillarvazquez@gmail.com>
2024-09-30 00:29:54 +0100

    fix: svgs changed to React components

--- src/components/logos/Bash.jsx
+++ src/components/logos/Bash.jsx
@@ -1,6 +1,28 @@
+import * as React from "react"
 
-export default function Bash() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 48 48"><g fill="none" stroke-linejoin="round" stroke-width="4"><rect width="40" height="32" x="4" y="8" fill="#2f88ff" stroke="#000" rx="2"/><path stroke="#fff" stroke-linecap="round" d="M12 18L19 24L12 30"/><path stroke="#fff" stroke-linecap="round" d="M23 32H36"/></g></svg>
-    )
+function SvgComponent(props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={50}
+      height={50}
+      viewBox="0 0 48 48"
+      {...props}
+    >
+      <g fill="none">
+        <rect
+          width={40}
+          height={32}
+          x={4}
+          y={8}
+          fill="#2f88ff"
+          stroke="#000"
+          rx={2}
+        />
+        <path stroke="#fff" d="M12 18l7 6-7 6M23 32h13" />
+      </g>
+    </svg>
+  )
 }
+
+export default SvgComponent
--- src/components/logos/NextJs.jsx
+++ src/components/logos/NextJs.jsx
@@ -1,6 +1,5 @@
+import React from 'react';
 
-export default function NextJs() {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 128 128"><circle cx="64" cy="64" r="64"/><path fill="url(#deviconNextjs0)" d="M106.317 112.014L49.167 38.4H38.4v51.179h8.614v-40.24l52.54 67.884a64 64 0 0 0 6.763-5.209"/><path fill="url(#deviconNextjs1)" d="M81.778 38.4h8.533v51.2h-8.533z"/><defs><linearGradient id="deviconNextjs0" x1="109" x2="144.5" y1="116.5" y2="160.5" gradientTransform="scale(.71111)" gradientUnits="userSpaceOnUse"><stop stopColor="#fff"/><stop offset="1" stopColor="#fff" stopOpacity="0"/></linearGradient><linearGradient id="deviconNextjs1" x1="121" x2="120.799" y1="54" y2="106.875" gradientTransform="scale(.71111)" gradientUnits="userSpaceOnUse"><stop stopXolor="#fff"/><stop offset="1" stopColor="#fff" stopOpacity="0"/></linearGradient></defs></svg>
-    )
+export default function LogosNextjsIcon(props) {
+	return (<svg xmlns="http://www.w3.org/2000/svg" width={50} height={50} viewBox="0 0 256 256" {...props}><defs><linearGradient id="logosNextjsIcon0" x1="55.633%" x2="83.228%" y1="56.385%" y2="96.08%"><stop offset="0%" stopColor="#fff"></stop><stop offset="100%" stopColor="#fff" stopOpacity={0}></stop></linearGradient><linearGradient id="logosNextjsIcon1" x1="50%" x2="49.953%" y1="0%" y2="73.438%"><stop offset="0%" stopColor="#fff"></stop><stop offset="100%" stopColor="#fff" stopOpacity={0}></stop></linearGradient><circle id="logosNextjsIcon2" cx={128} cy={128} r={128}></circle></defs><mask id="logosNextjsIcon3" fill="#fff"><use href="#logosNextjsIcon2"></use></mask><g mask="url(#logosNextjsIcon3)"><circle cx={128} cy={128} r={128}></circle><path fill="url(#logosNextjsIcon0)" d="M212.634 224.028L98.335 76.8H76.8v102.357h17.228V98.68L199.11 234.446a128 128 0 0 0 13.524-10.418"></path><path fill="url(#logosNextjsIcon1)" d="M163.556 76.8h17.067v102.4h-17.067z"></path></g></svg>);
 }
