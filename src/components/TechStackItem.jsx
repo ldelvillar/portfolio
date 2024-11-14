5055e3b938d64e678eb957e508230c22c3cde1d7
100495726 <ldelvillarvazquez@gmail.com>
2024-11-14 23:05:59 +0100

    refactor: change techstack items mapping

--- src/components/TechStackItem.jsx
+++ src/components/TechStackItem.jsx
@@ -1,7 +1,7 @@
 import Atropos from "atropos/react";
 import 'atropos/css';
 
-export default function TechStackItem({ title, array }) {
+export default function TechStackItem({ title, displayedTechnologies }) {
     return (
         <Atropos
             activeOffset={40}
@@ -10,7 +10,7 @@ export default function TechStackItem({ title, array }) {
             <div className="h-[100%] flex flex-col items-center bg-white dark:bg-primary border dark:border-gray-800 rounded-2xl">
                 <h2 className="text-3xl font-bold text-gray-100 bg-gradient-to-t from-[#fffc5f] to-[#c9b500] dark:from-[#fffec9] dark:to-[#efeb00] bg-clip-text text-transparent mt-3">{title}</h2>
                 <div className="my-6 grid grid-cols-3 gap-6">
-                    {array}
+                    {displayedTechnologies}
                 </div>
             </div>
         </Atropos>
--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -22,166 +22,115 @@ import Bash from '@/components/logos/Bash.jsx';
 import { nanoid } from "nanoid";
 import List from '@/components/icons/List';
 import AstroIcon from '@/components/logos/AstroIcon';
-import i18next from "i18next";
 
 export default function TechStack() {
-
-    const STACKS = {
-        FRONTEND: [
-            {
-                icon: <AstroIcon />,
-                name: 'Astro',
-                id: nanoid(),
-            },
-            {
-                icon: <ReactIcon />,
-                name: 'React',
-                id: nanoid(),
-            },
-            {
-                icon: <Tailwind />,
-                name: 'Tailwind CSS',
-                id: nanoid(),
-            },
-            {
-                icon: <Sass />,
-                name: 'SASS',
-                id: nanoid(),
-            },
-            {
-                icon: <Js />,
-                name: 'JavaScript',
-                id: nanoid(),
-            },
-            {
-                icon: <Figma />,
-                name: 'Figma',
-                id: nanoid(),
-            },
-        ],
-        BACKEND: [
-            {
-                icon: <Node />,
-                name: 'Node JS',
-                id: nanoid(),
-            },
-            {
-                icon: <NextJs />,
-                name: 'Next JS',
-                id: nanoid(),
-            },
-            {
-                icon: <PostgreSql />,
-                name: 'Postgre SQL',
-                id: nanoid(),
-            },
-            {
-                icon: <Firebase />,
-                name: 'Firebase',
-                id: nanoid(),
-            },
-        ],
-        AI: [
-            {
-                icon: <Python />,
-                name: 'Python',
-                id: nanoid(),
-            },
-            {
-                icon: <Tensorflow />,
-                name: 'Tensorflow',
-                id: nanoid(),
-            },
-            {
-                icon: <Keras />,
-                name: 'Keras',
-                id: nanoid(),
-            },
-            {
-                icon: <Numpy />,
-                name: 'NumPy',
-                id: nanoid(),
-            },
-            {
-                icon: <OpenCv />,
-                name: 'OpenCV',
-                id: nanoid(),
-            },
-        ],
-        OTHERS: [
-            {
-                icon: <Gcp /> ,
-                name: 'GCP',
-                id: nanoid(),
-            },
-            {
-                icon: <Azure /> ,
-                name: 'Azure',
-                id: nanoid(),
-            },
-            {
-                icon: <Git />,
-                name: 'Git',
-                id: nanoid(),
-            },
-            {
-                icon: <C /> ,
-                name: 'C',
-                id: nanoid(),
-            },
-            {
-                icon: <Linux />,
-                name: 'Linux',
-                id: nanoid(),
-            },
-            {
-                icon: <Bash />,
-                name: 'Terminal',
-                id: nanoid(),
-            },
-        ],
-    }
-
-    const frontend = STACKS.FRONTEND.map((item) => (
-        <div className="flex flex-col justify-center items-center text-gray-800 dark:text-gray-200" key={item.id}>
-            {/* div for the svg color */}
-            <div className='text-black dark:text-white'>
-                {item.icon}
-            </div>
-            <h3>{item.name}</h3>
-        </div>
-    ))
-
-    const backend = STACKS.BACKEND.map((item) => (
-        <div className="flex flex-col justify-center items-center text-gray-800 dark:text-gray-200" key={item.id}>
-            {/* div for the svg color */}
-            <div className='text-black dark:text-white'>
-                {item.icon}
-            </div>
-            <h3>{item.name}</h3>
-        </div>
-    ))
-
-    const ai = STACKS.AI.map((item) => (
-        <div className="flex flex-col justify-center items-center text-gray-800 dark:text-gray-200" key={item.id}>
-            {/* div for the svg color */}
-            <div className='text-black dark:text-white'>
-                {item.icon}
-            </div>
-            <h3>{item.name}</h3>
-        </div>
-    ))
-
-    const others = STACKS.OTHERS.map((item) => (
-        <div className="flex flex-col justify-center items-center text-gray-800 dark:text-gray-200" key={item.id}>
-            {/* div for the svg color */}
-            <div className='text-black dark:text-white'>
-                {item.icon}
-            </div>
-            <h3>{item.name}</h3>
-        </div>
-    ))
-
+    const FIELDS = [
+        {
+            name: "Frontend",
+            technologies: [
+                {
+                    icon: <AstroIcon />,
+                    name: 'Astro',
+                },
+                {
+                    icon: <ReactIcon />,
+                    name: 'React',
+                },
+                {
+                    icon: <Tailwind />,
+                    name: 'Tailwind CSS',
+                },
+                {
+                    icon: <Sass />,
+                    name: 'SASS',
+                },
+                {
+                    icon: <Js />,
+                    name: 'JavaScript',
+                },
+                {
+                    icon: <Figma />,
+                    name: 'Figma',
+                },
+            ],
+        },
+        {
+            name: "Backend",
+            technologies: [
+                {
+                    icon: <Node />,
+                    name: 'Node JS',
+                },
+                {
+                    icon: <NextJs />,
+                    name: 'Next JS',
+                },
+                {
+                    icon: <PostgreSql />,
+                    name: 'Postgre SQL',
+                },
+                {
+                    icon: <Firebase />,
+                    name: 'Firebase',
+                },
+            ],
+        },
+        {
+            name: "Machine Learning",
+            technologies: [
+                {
+                    icon: <Python />,
+                    name: 'Python',
+                },
+                {
+                    icon: <Tensorflow />,
+                    name: 'Tensorflow',
+                },
+                {
+                    icon: <Keras />,
+                    name: 'Keras',
+                },
+                {
+                    icon: <Numpy />,
+                    name: 'NumPy',
+                },
+                {
+                    icon: <OpenCv />,
+                    name: 'OpenCV',
+                },
+            ],
+        },
+        {
+            name: "Others",
+            technologies: [
+                {
+                    icon: <Gcp /> ,
+                    name: 'GCP',
+                },
+                {
+                    icon: <Azure /> ,
+                    name: 'Azure',
+                },
+                {
+                    icon: <Git />,
+                    name: 'Git',
+                },
+                {
+                    icon: <C /> ,
+                    name: 'C',
+                },
+                {
+                    icon: <Linux />,
+                    name: 'Linux',
+                },
+                {
+                    icon: <Bash />,
+                    name: 'Terminal',
+                },
+            ],
+        }
+    ];
+    
     return (
         <section id='techstack' className="scroll-m-20 mt-36 w-full mx-auto container lg:max-w-4xl md:max-w-2xl pb-10">
             <div className="mb-14">
@@ -193,24 +142,27 @@ export default function TechStack() {
                 </h2>
 
                 <div className="flex flex-col md:grid md:grid-cols-2 gap-5">
-                    <TechStackItem
-                        title="Frontend"
-                        array={frontend}
-                    />
-                    <TechStackItem
-                        title="Backend"
-                        array={backend}
-                    />
-                    <TechStackItem
-                        title={i18next.language === "en" ? "AI" : "IA"}
-                        array={ai}
-                    />
-                    <TechStackItem
-                        title={i18next.language === "en" ? "Others" : "Otros"}
-                        array={others}
-                    />
+                    {FIELDS.map(field => {
+                        const displayedTechnology = field.technologies.map((technology) => (
+                            <div className="flex flex-col justify-center items-center text-gray-800 dark:text-gray-200" key={nanoid()}>
+                                {/* div for the svg color */}
+                                <div className='text-black dark:text-white'>
+                                    {technology.icon}
+                                </div>
+                                <h3>{technology.name}</h3>
+                            </div>
+                        ));
+                        return (
+                            <TechStackItem
+                                title={field.name}
+                                displayedTechnologies={displayedTechnology}
+                                key={nanoid()}
+                            />
+                        );
+                    })}
+                    
                 </div>
             </div>
         </section>
-    )
+    );
 }
