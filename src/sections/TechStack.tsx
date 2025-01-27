848550ee3f9e8572572f2deef029a753b2549476
100495726 <ldelvillarvazquez@gmail.com>
2025-01-27 02:05:31 +0100

    update information about me

--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -1,30 +1,33 @@
-import TechStackItem from '@/components/TechStackItem';
 import { nanoid } from "nanoid";
 import List from '@/components/icons/List';
 import type { ReactNode } from 'react';
+import Atropos from "atropos/react";
+import 'atropos/css';
 
 import AstroIcon from '@/components/logos/AstroIcon';
 import ReactIcon from '@/components/logos/ReactIcon';
 import ReactRouter from '@/components/logos/ReactRouter';
-import Ts from '@/components/logos/TypescriptIcon';
+import TS from '@/components/logos/TypescriptIcon';
 import Tailwind from '@/components/logos/Tailwind';
 import Figma from '@/components/logos/Figma';
+import NextJS from '@/components/logos/NextJS';
 import Node from '@/components/logos/Node';
-import NextJs from '@/components/logos/NextJS';
-import PostgreSql from '@/components/logos/PostgreSql';
-import DeviconPhp from '@/components/logos/PHP';
+import Express from '@/components/logos/Express';
+import MySQL from '@/components/logos/MySQL';
+import PHP from '@/components/logos/PHP';
 import Firebase from '@/components/logos/Firebase';
 import Python from '@/components/logos/Python';
 import Tensorflow from '@/components/logos/Tensorflow';
 import Keras from '@/components/logos/Keras';
+import Pytorch from "@/components/logos/PyTorch";
 import Numpy from '@/components/logos/NumPy';
 import OpenCv from '@/components/logos/OpenCV';
 import Gcp from '@/components/logos/GCP';
 import Azure from '@/components/logos/Azure';
 import Git from '@/components/logos/Git';
 import C from '@/components/logos/C';
+import Java from '@/components/logos/Java';
 import Linux from '@/components/logos/Linux';
-import Bash from '@/components/icons/Bash';
 
 interface Technology {
     icon: ReactNode;
@@ -53,7 +56,7 @@ const FIELDS: TechField[] = [
                 name: 'React Router',
             },
             {
-                icon: <Ts />,
+                icon: <TS />,
                 name: 'TypeScript',
             },
             {
@@ -70,24 +73,28 @@ const FIELDS: TechField[] = [
         name: "Backend",
         technologies: [
             {
+                icon: <NextJS />,
+                name: 'Next JS',
+            },
+            {
                 icon: <Node />,
                 name: 'Node JS',
             },
             {
-                icon: <NextJs />,
-                name: 'Next JS',
+                icon: <Express />,
+                name: 'Express',
             },
             {
-                icon: <PostgreSql />,
-                name: 'Postgre SQL',
-            },
-            {
-                icon: <DeviconPhp />,
-                name: 'PHP',
+                icon: <MySQL />,
+                name: 'MySQL',
             },
             {
                 icon: <Firebase />,
                 name: 'Firebase',
+            },
+            {
+                icon: <PHP />,
+                name: 'PHP',
             },
         ],
     },
@@ -105,6 +112,10 @@ const FIELDS: TechField[] = [
             {
                 icon: <Keras />,
                 name: 'Keras',
+            },
+            {
+                icon: <Pytorch />,
+                name: 'PyTorch',
             },
             {
                 icon: <Numpy />,
@@ -136,12 +147,12 @@ const FIELDS: TechField[] = [
                 name: 'C',
             },
             {
-                icon: <Linux />,
-                name: 'Linux',
+                icon: <Java />,
+                name: 'Java',
             },
             {
-                icon: <Bash />,
-                name: 'Terminal',
+                icon: <Linux />,
+                name: 'Linux',
             },
         ],
     }
@@ -162,7 +173,6 @@ export default function TechStack() {
                     {FIELDS.map(field => {
                         const displayedTechnology = field.technologies.map((technology) => (
                             <div className="flex flex-col justify-center items-center text-gray-800 dark:text-gray-200" key={nanoid()}>
-                                {/* div for the svg color */}
                                 <div className='text-black dark:text-white'>
                                     {technology.icon}
                                 </div>
@@ -170,14 +180,20 @@ export default function TechStack() {
                             </div>
                         ));
                         return (
-                            <TechStackItem
-                                title={field.name}
-                                displayedTechnologies={displayedTechnology}
+                            <Atropos
+                                activeOffset={40}
+                                shadowScale={1.05}
                                 key={nanoid()}
-                            />
+                            >
+                                <div className="h-[100%] flex flex-col items-center bg-white dark:bg-primary border dark:border-gray-800 rounded-2xl">
+                                    <h2 className="text-3xl font-bold text-gray-100 bg-gradient-to-t from-[#fffc5f] to-[#c9b500] dark:from-[#fffec9] dark:to-[#efeb00] bg-clip-text text-transparent mt-3">{field.name}</h2>
+                                    <div className="my-6 grid grid-cols-3 gap-6">
+                                        {displayedTechnology}
+                                    </div>
+                                </div>
+                            </Atropos>
                         );
                     })}
-                    
                 </div>
             </div>
         </section>
