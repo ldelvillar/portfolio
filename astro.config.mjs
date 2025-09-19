991af90939b043f18aefa835af53ab75caf198ba
ldelvillar <ldelvillarvazquez@gmail.com>
2025-09-19 11:29:36 +0200

    refactor: replace tailwind.config file by global.css and load fonts locally

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   redirects: {
     '/': '/es',
   },
-  integrations: [tailwind()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
