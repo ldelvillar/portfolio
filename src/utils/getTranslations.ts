27e055a14ec3f2d3e9c43de1426c4f59ab3fa187
ldelvillar <ldelvillarvazquez@gmail.com>
2026-05-12 22:04:22 +0200

    fix(i18n): update locale type to reuse existing one

--- src/utils/getTranslations.ts
+++ src/utils/getTranslations.ts
@@ -1,6 +1,5 @@
-// src/utils/getTranslations.ts
-type Locale = 'en' | 'es';
+import type { Lang } from '@/i18n/config';
 
-export async function getTranslations(locale: Locale) {
+export async function getTranslations(locale: Lang) {
   return await import(`../i18n/${locale}.json`);
 }
